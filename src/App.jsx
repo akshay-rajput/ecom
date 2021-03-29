747e94d9f7b547393f1a86cbfb919e47fd55db8c
Akshay <akshayrajput102@gmail.com>
2021-03-29 21:58:58 +0530

    App.jsx - feat - import components, add tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,40 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
+// import logo from './logo.svg'
 import './App.css'
+import products from './store/generate_products'
+import TheNavbar from './components/TheNavbar'
+import TheFooter from './components/TheFooter'
+import ProductListing from './components/ProductListing'
+import Cart from './components/Cart'
+import Wishlist from './components/Wishlist'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfProducts, setListOfProducts] = useState([])
+  const [activeTab, setActiveTab] = useState('ProductListing');
+
+  useEffect(()=> {
+    setListOfProducts(prevList => ( [...prevList] , products ))
+    console.log('generating..', products.length)
+    console.log('list: ', listOfProducts);
+  }, [])
+
+  function handleTabChange(tabname){
+    setActiveTab(tabname);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main className="App textGray1">
+      <TheNavbar changeTab = {handleTabChange} ></TheNavbar>
+
+      <div className='app-content containerMid pt4 pb4 flexGrow'>
+        {activeTab == 'ProductListing' && <ProductListing listOfProducts={listOfProducts} />}
+        {activeTab == 'Cart' && <Cart />}
+        {activeTab == 'Wishlist' && <Wishlist />}
+      </div>
+
+      {/* footer */}
+      <TheFooter></TheFooter>
+    </main>
   )
 }
 
