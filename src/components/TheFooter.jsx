2b682d46606360099725fb06316e60b8037630c0
Akshay <akshayrajput102@gmail.com>
2021-04-12 19:37:52 +0530

    feat - footer - added footer v1

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function TheFooter() {
     return (
-        <footer className='mt8'>
+        <footer className='mt8 md:mb0' id='footer'>
             <hr className='border borderGray5'/>
             <div className='containerMid textCenter textSm textGray4 pt2 pb2'>
-                Made by <a href="https://github.com/akshay-rajput" rel='noopener noreferrer' target='_blank' className="textTeal4 hover:textTeal2">Akshay</a>
+                <i className="fas fa-code"></i> by <a href="https://github.com/akshay-rajput" rel='noopener noreferrer' target='_blank' className="textTeal4 hover:textTeal2">Akshay</a>
             </div>
         </footer>
     )
