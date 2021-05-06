9765ccf5d62d0872721e690fff861db4098d2f39
Akshay <akshayrajput102@gmail.com>
2021-05-06 23:53:47 +0530

    feat - productcard - use link for name, render ui based on db product

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
 import {checkExistanceInArray} from '../store/checkExistanceInArray'
 import {userActions, createUserAction, userDataContext} from '../reducers/userDataReducer'
 
@@ -11,19 +12,19 @@ export default function ProductCard({product, changeTab}) {
     
     function addToCart(item){
         console.log('dispatched add to cart with ', item);
-        if(!checkExistanceInArray(userDataState.cartItems, item.id)){
+        if(!checkExistanceInArray(userDataState.cartItems, item._id)){
             item.qty = 1;
             userDataDispatch(createUserAction(userActions.ADD_TO_CART, item))
             userDataDispatch(createUserAction(userActions.FIND_CARTTOTAL))
         }
-        else{
-            // open cart
-            changeTab('Cart')
-        }
+        // else{
+        //     // open cart
+        //     // changeTab('Cart')
+        // }
     }
     function addToWishlist(item){
         // if not in wishlist, add
-        if(!checkExistanceInArray(userDataState.wishlist, item.id)){
+        if(!checkExistanceInArray(userDataState.wishlist, item._id)){
             userDataDispatch(createUserAction(userActions.ADD_TO_WISHLIST, item))
         }
         else{
@@ -32,31 +33,37 @@ export default function ProductCard({product, changeTab}) {
     }
 
     return (
-        <div className='displayFlex flexCol border borderTeal5 rounded p2'>
-            <label className='textSm mb2'>{product.name}</label>
-            <div className='image-wrapper flexGrow'>
-                <img src={product.image} alt={product.name} className="wFull rounded mb2"/>
+        <div className='product-card displayFlex flexCol border borderTeal5 rounded pb2'>
+            <div className='image-wrapper displayFlex itemsCenter justifyCenter rounded p2'>
+                <img src={product.images} alt={product.name} className="product-card-image mb2"/>
             </div>
-            <span className="mb2 fontSemiBold">$ {product.price}</span>
-            <div className="textXs mb2 displayFlex flexWrap justifyBetween itemsCenter ">
-                <span className="">GST: {product.gst}</span>
-                <span className="">{product.inStock ? 'In stock': 'Out of stock'}</span>
-            </div>
-            <div className="product-actions displayGrid gridCols4 gridGap1 mt2">
-                {
-                    checkExistanceInArray(userDataState.cartItems, product.id) ?
-                    <button onClick={()=> addToCart(product)} className=" p1 bgGray5 border borderTeal3 gridColSpan3 md:gridColSpan3 textWhite rounded">
-                        Go to Cart
+            
+            <div className="product-data flexGrow displayFlex flexCol justifyBetween">
+                <div className="product-info displayFlex flexCol">
+                    <Link to={`/product/${product._id}`} className="pl2 pr2 textWhite hover:textTeal4 mb2">{product.name}</Link>
+                    
+                    <span className="pl2 pr2 mb2 fontSemiBold">&#8377; {product.price}</span>
+                    <div className="pl2 pr2 textXs mb2 displayFlex flexWrap justifyBetween itemsCenter ">
+                        <span className="">Category: {product.category}</span>
+                        <span className="">Rating: {product.rating}</span>
+                    </div>
+                </div>
+                <div className="pl2 pr2 product-actions displayGrid gridCols4 gridGap1 mt2">
+                    {
+                        checkExistanceInArray(userDataState.cartItems, product._id) ?
+                        <Link to='/cart' className="textCenter p1 bgGray5 border borderTeal3 gridColSpan3 md:gridColSpan3 textWhite rounded">
+                            Go to Cart
+                        </Link>
+                        : 
+                        <button onClick={()=> addToCart(product)} className=" p1 bgTeal6 hover:bgTeal5 border borderTeal3 gridColSpan3 md:gridColSpan3 textWhite rounded">
+                            Add to cart
+                        </button>
+                    }
+                    
+                    <button onClick={() => addToWishlist(product)} className='displayFlex justifyCenter itemsCenter bgTransparent border borderTeal4 gridColSpan1 md:gridColSpan1 rounded p2'>
+                        <i className={checkExistanceInArray(userDataState.wishlist, product._id) ? 'fas fa-heart textRed3 textRg': 'far fa-heart textRed3 textRg'}></i>
                     </button>
-                    : 
-                    <button onClick={()=> addToCart(product)} className=" p1 bgTeal6 hover:bgTeal5 border borderTeal3 gridColSpan3 md:gridColSpan3 textWhite rounded">
-                        Add to cart
-                    </button>
-                
-                }
-                <button onClick={() => addToWishlist(product)} className='displayFlex justifyCenter itemsCenter bgTransparent border borderTeal4 gridColSpan1 md:gridColSpan1 rounded p2'>
-                    <i className={checkExistanceInArray(userDataState.wishlist, product.id) ? 'fas fa-heart textRed3 textRg': 'far fa-heart textRed3 textRg'}></i>
-                </button>
+                </div>
             </div>
         </div>
     )
