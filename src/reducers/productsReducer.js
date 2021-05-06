d346058a06628cc16568b160176eca137c7b23fb
Akshay <akshayrajput102@gmail.com>
2021-05-06 23:51:59 +0530

    refactor - productReducer - change actions to filter

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -5,10 +5,12 @@ export const StoreContext = React.createContext(null);
 
 // action constants object
 export const actions = {
+    INITIALISE_PRODUCTS: 'initialise_products',
     SORT_PRICE: 'sort_price',
     SHOW_ALL_PRODUCTS: 'show_all_products',
-    SHOW_GSTFREE: 'show_gstfree',
-  
+    SHOW_BOOKS: 'show_books',
+    SHOW_FLASHCARDS: 'show_flashcards',
+    SHOW_TOYS: 'show_toys',
 }
 
 // helper function to pass object to dispatch
@@ -20,21 +22,37 @@ export const createAction = (type, payload)=> {
 
 // reducer function which accepts state and action
 export const productReducer = (state, action) => {
+    console.log("called productReducer: Action - ", action.payload)
     switch (action.type) {
+        case actions.INITIALISE_PRODUCTS:
+            return {
+                ...state, 
+                productList: action.payload
+            };
         case actions.SHOW_ALL_PRODUCTS:
             return {
                 ...state, 
-                show_all_products: !state.show_all_products
+                productAppliedFilter: "All"
             };
         case actions.SORT_PRICE:
             return {
                 ...state, 
                 sort_price: action.payload
             };
-        case actions.SHOW_GSTFREE:
+        case actions.SHOW_BOOKS:
             return {
                 ...state, 
-                show_gstfree: !state.show_gstfree
+                productAppliedFilter: "Books"
+            };
+        case actions.SHOW_FLASHCARDS:
+            return {
+                ...state, 
+                productAppliedFilter: 'Flashcards'
+            };
+        case actions.SHOW_TOYS:
+            return {
+                ...state, 
+                productAppliedFilter: "Toys"
             };
         default:
             return state;
