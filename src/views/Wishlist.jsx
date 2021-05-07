c6e644aeaffadeef9f6c9fb771e582ac11b23be1
Akshay <akshayrajput102@gmail.com>
2021-05-07 22:01:38 +0530

    style - wishlist - change card layout

--- src/views/Wishlist.jsx
+++ src/views/Wishlist.jsx
@@ -40,33 +40,46 @@ export default function Wishlist() {
 
     return (
         <>
-            <h4 className='textLg fontMedium'>My Wishlist <span className='textMd textGray4'>({state.wishlist.length})</span></h4>
+            <div className="displayFlex justifyBetween itemsCenter">
+                <h4 className='textLg fontMedium'>My Wishlist <span className='textMd textGray4'>({state.wishlist.length})</span></h4>
+                <button onClick={() => history.back()} className="pl4 pr4 pt2 pb2 rounded bgGray5 textWhite borderNone">
+                    <i className="fas fa-angle-left"></i> Back
+                </button>
+            </div>
             <hr className='border borderGray5 mt4 mb4'/>
             {
                 state.wishlist.length > 0 &&
-                <div className="displayGrid gridCols2 md:gridCols5 gridGap4 pt4 pb4">
+                <div className="displayGrid gridCols1 md:gridCols3 gridGap4 pt4 pb4">
                 {
                     state.wishlist.map(product => {
                     return (
-                        <div key={product._id} className='displayFlex flexCol border rounded p2'>
-                            <div className='image-wrapper flexGrow'>
-                                <img src={product.images} alt={product.name} className="wFull rounded mb2"/>
+                        <div key={product._id} className='displayGrid gridCols3 border rounded p2'>
+                            <div className='image-wrapper gridColSpan1'>
+                                <img src={product.images} alt={product.name} className="wFull rounded"/>
                             </div>
                             
-                            <Link to={`/product/${product._id}`} className="textWhite hover:textTeal4 mb2">{product.name}</Link>
+                            <div className="displayFlex flexCol gridColSpan2 pr2 pl4">
+                                <div className="flexGrow displayFlex flexCol justifyBetween">
+                                    <Link to={`/product/${product._id}`} className="textWhite hover:textTeal4 mb2">{product.name}</Link>
                             
-                            <span className="mb2 fontSemiBold">$ {product.price}</span>
-                            <div className="textXs mb2 displayFlex flexWrap justifyBetween itemsCenter ">
-                                <span className="">Category: {product.category}</span>
-                                <span className="">Rating: {product.rating} / 5</span>
-                            </div>
-                            <div className="product-actions displayGrid gridCols7 gridGap1 mt2">
+                                    <div className="">
+                                        <span className="mb2 displayBlock fontSemiBold">&#8377; {product.price}</span>
+                                        <div className="textXs mb2 displayFlex flexWrap justifyBetween itemsCenter ">
+                                            <span className="">Category: {product.category}</span>
+                                            <span className="">Rating: {product.rating} / 5</span>
+                                        </div>
+                                    </div>
+                                </div>
                                 
-                                <button onClick={()=> moveToCart(product)} className="p1 bgTeal6 hover:bgTeal5 border borderTeal3 gridColSpan5 md:gridColSpan6 textWhite rounded">Move to Cart</button>
-                                
-                                <button onClick={() => addToWishlist(product)} className='bgTransparent border borderTeal4 gridColSpan2 md:gridColSpan1 rounded p2'>
-                                    <i className={checkExistanceInArray(state.wishlist, product._id) ? 'fas fa-heart textRed3 textRg': 'far fa-heart textRed3 textRg'}></i>
-                                </button>
+                                <div className="product-actions displayGrid gridCols7 gridGap1 mt2">
+                                    
+                                    <button onClick={()=> moveToCart(product)} className="p1 bgTeal6 hover:bgTeal5 border borderTeal3 gridColSpan5 textWhite rounded">Move to Cart</button>
+                                    
+                                    <button onClick={() => addToWishlist(product)} className='bgTransparent border borderTeal4 gridColSpan2 rounded pt2 pb2'>
+                                        <i className={checkExistanceInArray(state.wishlist, product._id) ? 'fas fa-heart textRed3 textRg': 'far fa-heart textRed3 textRg'}></i>
+                                    </button>
+                                </div>
+
                             </div>
                         </div>
                     )
