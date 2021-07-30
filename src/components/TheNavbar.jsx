f4aefa08572afc917cf39023f3b27e37689d1278
Akshay <akshayrajput102@gmail.com>
2021-07-30 17:31:31 +0530

    feat - navbar - show active link

--- src/components/TheNavbar.jsx
+++ src/components/TheNavbar.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {userDataContext} from '../reducers/userDataReducer'
 
 
@@ -13,42 +13,24 @@ export default function TheNavbar({changeTab, activeTab}) {
             <nav className="app-navbar">
                 <div className=" containerMid displayFlex justifyBetween itemsCenter pt2 pb2">
                     
-                    <NavLink to="/" className="textTeal3 hover:textTeal5 fontSemiBold textMd p1 mr4">Wisp</NavLink>
+                    <NavLink end activeClassName="active-link" to="/" className="textTeal3 hover:textTeal5 fontSemiBold textMd p1 mr4">Wisp</NavLink>
                     
                     <ul className="displayFlex textRg">
-                        <NavLink exact={true?'true': 'false'}  to="/" activeClassName="active" className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1"}>
+                        <NavLink end to="/" activeClassName="active-link" className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1"}>
                             <i className="fas fa-home textMd"></i>
                         </NavLink>
 
-                        <NavLink to="/cart" activeClassName="active" className={"btn-cart bgTransparent borderNone textRg p2 ml2 mr2 textTeal4 hover:textTeal1"}>
+                        <NavLink to="/cart" activeClassName="active-link" className={"btn-cart bgTransparent borderNone textRg p2 ml2 mr2 textTeal4 hover:textTeal1"}>
                             <i className="fas fa-shopping-bag textMd"></i>
                             {
                                 state.cartItems.length > 0 && <span className="count">{state.cartItems.length}</span>
                             }
                         </NavLink>
                         
-                        <NavLink to="/wishlist" activeClassName="active" className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1"}>
+                        <NavLink to="/wishlist" activeClassName="active-link" className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1"}>
                             <i className="fas fa-heart textMd"></i>
                         </NavLink>
                         
-                        {/* <li className="listNoStyle">
-                            <button onClick={()=> changeTab('ProductListing')} className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1" + (activeTab == 'ProductListing'? ' active': '')}>
-                                <i className="fas fa-home textMd"></i>
-                            </button>
-                        </li>
-                        <li className="listNoStyle ml2 mr2 md:mr8 md:ml8">
-                            <button onClick={()=> changeTab('Cart')} className={"btn-cart bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1" + (activeTab == 'Cart'? ' active': '')}>
-                                <i className="fas fa-shopping-bag textMd"></i>
-                                {
-                                    state.cartItems.length > 0 && <span className="count">{state.cartItems.length}</span>
-                                }
-                            </button>
-                        </li>
-                        <li className="listNoStyle">
-                            <button onClick={()=> changeTab('Wishlist')} className={"bgTransparent borderNone textRg p2 textTeal4 hover:textTeal1" + (activeTab == 'Wishlist'? ' active': '')}>
-                                <i className="fas fa-heart textMd"></i>
-                            </button>
-                        </li> */}
                     </ul>
                 </div>
             </nav>
