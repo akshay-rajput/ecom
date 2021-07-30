ab40ef8a2d1b0640c033cd4b11c6d7220788cd17
Akshay <akshayrajput102@gmail.com>
2021-07-30 17:31:06 +0530

    feat - 404 - handle page not found

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div className="displayFlex flexCol itemsCenter justifyCenter pt12 pb12 pl4 pr4 mt12">
+            Page you're looking for not found
+        </div>
+    )
+}
